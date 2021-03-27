e1743d3a49bcb44d1398ee127572ecf7a504e6ef
hyird <hyird@outlook.com>
2021-03-28 02:13:55 +0800

    add asterisk

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -6,13 +6,11 @@ rm -rf feeds/packages/libs/libcap
 rm -rf package/feeds/routing/olsrd/patches/012-gpsd.patch
 rm -rf feeds/packages/net/smartdns
 rm -rf package/lean/luci-theme-argon
-rm -rf feeds/packages/net/miniupnpd
 
 
 svn co https://github.com/openwrt/packages/trunk/libs/libcap                                             feeds/packages/libs/libcap
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome                          package/adg
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns                                     package/smartdns
-svn co https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng/trunk/miniupnpd                            package/miniupnpd
 
 git clone https://github.com/pymumu/luci-app-smartdns.git -b lede                                        package/luci-app-smartdns
 git clone https://github.com/hyird/Openwrt-AdguardHome.git                                               package/AdGuardHome
@@ -20,7 +18,7 @@ git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git    
 git clone https://github.com/yangsongli/luci-theme-atmaterial.git                                        package/luci-theme-atmaterial
 git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06                                     package/luci-theme-argon-jerrykuku
 git clone https://github.com/tty228/luci-app-serverchan.git                                              package/luci-app-serverchan
-git clone https://github.com/vernesong/OpenClash.git -b master                                           package/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git -b master  --depth=1                                package/luci-app-openclash
 git clone https://github.com/tindy2013/openwrt-subconverter.git                                          package/openwrt-subconverter
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git                                         package/luci-theme-opentomcat
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git                                         package/luci-theme-opentomato
@@ -29,6 +27,7 @@ git clone https://github.com/jerrykuku/lua-maxminddb.git                        
 git clone https://github.com/jerrykuku/luci-app-vssr.git                                                 package/luci-app-vssr
 git clone https://github.com/hyird/luci-theme-darkmatter.git		                                     package/luci-theme-darkmatter
 git clone https://github.com/garypang13/luci-app-dnsfilter.git                                           package/luci-app-dnsfilter
+
 
 sed -i 's/DEPENDS.*/& \+adguardhome/g'  package/adg/Makefile
 sed -i 's/DEPENDS.*/& \+kmod-tun +libcap-bin/g'  package/luci-app-openclash/luci-app-openclash/Makefile
